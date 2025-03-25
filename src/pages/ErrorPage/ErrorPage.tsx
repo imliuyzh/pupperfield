2df9a8d142b042d21be25e1321cbb112d6095987
Yizhen Liu <imliuyzh@outlook.com>
2025-03-25 17:01:34 +0000

    Revise

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -5,10 +5,10 @@ export default function ErrorPage() {
   return (
     <main className="bg-[#300d38] flex flex-col min-h-[100vh] justify-between min-w-full relative">
       <h1 className="pb-0 pt-0 tagline text-white">
-        <span className="block">We can't find</span>
+        <span className="block">We can&apos;t find</span>
         <span className="block">the page.</span>
         <span className="block">Do you want to</span>
-        <span className="font-black text-[var(--background)] tracking-[-20px] uppercase">
+        <span className="font-bold text-[var(--background)] tracking-[-20px] uppercase">
           <Link to="/">go back?</Link>
         </span>
       </h1>
