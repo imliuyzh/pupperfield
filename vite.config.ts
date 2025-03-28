eb92e2a89b7b98cf9802e5032e451f959c02108f
Yizhen Liu <imliuyzh@outlook.com>
2025-03-28 05:38:36 -0700

    Revise

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.Node_ENV === "production" ? "/pupperfield/" : "/",
+  base: "/pupperfield/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
